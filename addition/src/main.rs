0117fbf097435323ffaf2622f01af359244dd166
Dacio Romero <dacioromero@gmail.com>
2020-04-17 20:18:57 -0700

    Initial commit

---
+++ addition/src/main.rs
@@ -0,0 +1,11 @@
+use std::env;
+
+fn main() {
+  let mut total: i128 = 0;
+
+  for arg in env::args().skip(1) {
+    total += arg.parse::<i128>().unwrap();
+  }
+
+  println!("The total is {}", total);
+}
---
+++ server/src/main.rs
@@ -0,0 +1,20 @@
+use actix_web::{get, Responder, HttpResponse, HttpServer, App};
+
+#[get("/")]
+async fn home() -> impl Responder {
+    HttpResponse::Ok().body("Ok")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let bind = "0.0.0.0:3000";
+    println!("Starting server on {}", bind);
+
+    HttpServer::new(|| {
+        App::new()
+            .service(home)
+    })
+    .bind(bind)?
+    .run()
+    .await
+}
